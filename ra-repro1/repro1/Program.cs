b8fa835da016e3ba1cece5c838c851fc7ba14d67
Wei Zheng <13881045+wzchua@users.noreply.github.com>
2021-07-11 10:38:58 +0800

    Add repro

---
+++ ra-repro1/repro1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace repro1
+{
+    public readonly struct S
+    {
+        public readonly int Field;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IReadOnlyList<object> list = new List<object>();
+            var a = list.FirstOrDefault();
+            Console.WriteLine("Hello World!");
+        }
+        public async void Test()
+        {
+            await Task.Delay(10);
+
+        }
+    }
+}
